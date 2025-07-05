2e8aefe1086434cdfb9ef8da29843ce8fb4872a6
vishawanath <vishu.bengaluru@gmail.com>
2025-07-05 09:42:35 +0530

    added changes for circuit breaker design pattern

--- src/main/java/com/strugglertoachiever/booking/controller/BookingController.java
+++ src/main/java/com/strugglertoachiever/booking/controller/BookingController.java
@@ -1,5 +1,6 @@
 package com.strugglertoachiever.booking.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,15 @@ public class BookingController {
     private RestTemplate restTemplate;
 
     @GetMapping("/hotel-ping")
+    @CircuitBreaker(name = "hotelServiceCB", fallbackMethod = "hotelServiceFallback")
     public String callHotelService() {
         String hotelServiceResponse = restTemplate.getForObject("http://HOTELSERVICE/hotels/ping", String.class);
         return "Response from HotelService: " + hotelServiceResponse;
+    }
+
+    // Fallback method signature must match the main method plus Throwable at the end
+    public String hotelServiceFallback(Throwable t) {
+        return "Hotel service is unavailable (from Circuit Breaker fallback). Please try again later.";
     }
 
     @GetMapping("/ping")
