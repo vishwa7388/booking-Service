1983b7d5df2feeb6278db6a7ff686703ec42cd13
vishawanath <vishu.bengaluru@gmail.com>
2025-07-08 10:53:13 +0530

    Configured MySQL and updated booking service files

--- src/main/java/com/strugglertoachiever/booking/config/RestTemplateConfig.java
+++ src/main/java/com/strugglertoachiever/booking/config/RestTemplateConfig.java
@@ -1,8 +1,13 @@
 package com.strugglertoachiever.booking.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,5 +17,11 @@ public class RestTemplateConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ReactorServiceInstanceLoadBalancer randomLoadBalancer(Environment env, LoadBalancerClientFactory factory) {
+        String serviceId = env.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(factory.getLazyProvider(serviceId, ServiceInstanceListSupplier.class), serviceId);
     }
 }
--- src/main/java/com/strugglertoachiever/booking/controller/BookingController.java
+++ src/main/java/com/strugglertoachiever/booking/controller/BookingController.java
@@ -1,8 +1,12 @@
 package com.strugglertoachiever.booking.controller;
 
+import io.github.resilience4j.bulkhead.BulkheadFullException;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,9 @@ public class BookingController {
     private CircuitBreakerRegistry circuitBreakerRegistry;
 
     @GetMapping("/hotel-ping")
+    @Bulkhead(name = "hotelServiceBulkhead", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "hotelServiceFallback")
+    @Retry(name = "hotelServiceRetry", fallbackMethod = "hotelServiceRetryFallback")
+    @RateLimiter(name = "hotelServiceRateLimiter", fallbackMethod = "hotelFallback")
     public String callHotelService() {
         CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker("hotelServiceCB");
 
@@ -45,5 +52,17 @@ public class BookingController {
     @GetMapping("/ping")
     public String ping() {
         return "Booking Service is up";
+    }
+
+    public String hotelServiceFallback(BulkheadFullException ex) {
+        return "Hotel Service is busy; please try later.";
+    }
+
+    public String hotelServiceRetryFallback(Exception ex) {
+        return "Hotel Service is temporarily unavailable. Please try again later.";
+    }
+
+    public String hotelFallback(Throwable t) {
+        return "Hotel service is busy, please try again later.";
     }
 }
